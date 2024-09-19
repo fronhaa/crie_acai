a37f27aff4ea5b40dd73f9a2fa3fb19811f9e3ef
Marcelo Martiniano <marcelo@webjasper.com.br>
2024-09-18 23:18:20 -0300

    PUSH URL SERVER

--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -1,4 +1,4 @@
-const apiUrl = 'http://3.19.232.76';
+const apiUrl = 'http://3.19.232.76/api';
 
 const btnSubmeter = document.querySelector('#btn-submit');
 let divAlertWarning = document.querySelector('.alert-warning') || document.querySelector('.alert-success');
